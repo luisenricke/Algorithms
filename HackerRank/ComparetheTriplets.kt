44cd5bbfe28f12261fd97a54def89e62b55bf6f6
luisenricke <36013173+luisenricke@users.noreply.github.com>
2019-03-13 00:53:31 -0600

    complete: Comparete The Triples

---
+++ HackerRank/ComparetheTriplets.kt
@@ -0,0 +1,18 @@
+fun compareTriplets(x: Array<Int>, y: Array<Int>): Array<Int> {
+    var points: Array<Int> = arrayOf(0, 0)
+    for (count in 0..2) {
+        if (x[count] > y[count]) points[0]++
+        if (x[count] < y[count]) points[1]++
+        if (x[count] == y[count]) continue
+    }
+    return points
+}
+
+fun main() {
+
+    var bob = Array<Int>(3) { readLine()!!.toInt() }
+    var alice = Array<Int>(3) { readLine()!!.toInt() }
+    val result = compareTriplets(bob,alice)
+    println(result.joinToString(" "))
+
+}
